4503703653b31ec3c75989c2ff368f75e79f96e4
marouan-folane <marouanfolane@gmail.com>
2025-08-02 12:35:21 +0100

    mylead

--- src/Unlock.jsx
+++ src/Unlock.jsx
@@ -6,7 +6,7 @@ const LockerIframe = () => {
      
 
       <iframe
-        src="https://optilinklock.com/1830709"
+        src="https://bestlocker.eu/iframe/e1ae7cea-6f92-11f0-9d00-8a5fb7be40ea"
         title="Content Locker"
         width="100%"
         height="600px"
